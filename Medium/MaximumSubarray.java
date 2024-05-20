f93525a18615613172946ddf025b8dd64a77aec6
VISHNU KUMAR S R <146939749+vishnukumarsr21@users.noreply.github.com>
2024-05-20 20:56:08 +0530

    Create MaximumSubarray.java

---
+++ Medium/MaximumSubarray.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        
+        Solution solution = new Solution();
+        int result = solution.maxSubArray(nums);
+        
+        System.out.println("The maximum subarray sum is: " + result);
+        
+        scanner.close();
+    }
+}
